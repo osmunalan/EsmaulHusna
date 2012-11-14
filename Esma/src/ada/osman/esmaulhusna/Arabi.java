d5139dbc22c51c8f9f7c87653c71f5d0d8ec1c54
OSman <osman@osman-pc.(none)>
2012-11-14 18:36:15 +0200

    Arabi

---
+++ Esma/src/ada/osman/esmaulhusna/Arabi.java
@@ -0,0 +1,13 @@
+package ada.osman.esmaulhusna;
+
+import ada.osman.esmaulhusna.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Arabi extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fusus_layout);
+	}
+}
